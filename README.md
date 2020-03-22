64163968aa414620a76fb6d9a8dec8157d8f0cf5
Kuba <qubatron15@gmail.com>
2020-03-22 16:24:00 +0100

    lazy loading with usage of SED

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HomeSurveillanceSystem
 Configuration for Ubuntu to use PC as monitoring system for safety purpouses.
+
+
+crontab configuration (sudo crontab -e)
+@reboot /home/monitoring/HomeSurveillanceSystem/code/crontab-tasks.sh
+30 */1 * * * /home/monitoring/HomeSurveillanceSystem/code/gallery-generation.sh 
--- code/gallery-generation.sh
+++ code/gallery-generation.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 
-llgal -w 10 -R -d /home/monitoring/Kamery/
+llgal --sort revname -w 10 -R -d /home/monitoring/Kamery/
+sed -i 's/<img src=/<img src="http:\/\/192.168.0.150:8085\/photo-placeholder.svg" data-src=/g' /home/monitoring/Kamery/Kamera1/index.html
+sed -i 's/<img src=/<img src="http:\/\/192.168.0.150:8085\/photo-placeholder.svg" data-src=/g' /home/monitoring/Kamery/Kamera2/index.html
+sed -i 's/<img src=/<img src="http:\/\/192.168.0.150:8085\/photo-placeholder.svg" data-src=/g' /home/monitoring/Kamery/Kamera3/index.html
